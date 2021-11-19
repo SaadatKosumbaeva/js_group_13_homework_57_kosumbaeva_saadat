9e1696015cac6481b915a68ea967432f8164e0f8
Saadat Kosumbaeva <kosumbaevasaadat@gmail.com>
2021-11-19 11:18:03 +0600

    Created basic app structure.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { NewUserComponent } from './new-user/new-user.component';
+import { UsersComponent } from './users/users.component';
+import { UserComponent } from './users/user/user.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NewUserComponent,
+    UsersComponent,
+    UserComponent
   ],
   imports: [
     BrowserModule
